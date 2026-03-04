00346c597c52b886da0c97caf313b8996539cb36
Deivi De la Vega <deividelavegab@gmail.com>
2026-03-04 01:25:33 -0500

    SearchBar Desing 🚀 (#5)

--- app/src/main/java/com/dev/moviesapp/presentation/ui/movies/list/components/SearchBar.kt
+++ app/src/main/java/com/dev/moviesapp/presentation/ui/movies/list/components/SearchBar.kt
@@ -1,65 +1,50 @@
 package com.dev.moviesapp.presentation.ui.movies.list.components
 
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.rememberScrollState
-import androidx.compose.foundation.verticalScroll
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Search
+import androidx.compose.material3.Icon
 import androidx.compose.runtime.Composable
-import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.ListItem
-import androidx.compose.material3.SearchBar
-import androidx.compose.material3.SearchBarDefaults
 import androidx.compose.material3.Text
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.saveable.rememberSaveable
-import androidx.compose.runtime.setValue
-import androidx.compose.ui.Alignment
+import androidx.compose.material3.TextField
+import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
 
-@OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun SimpleSearchBarPreview() {
-    var expanded by rememberSaveable { mutableStateOf(false) }
-
+fun ModernSearchBar(
+    query: String,
+    onQueryChange: (String) -> Unit
+) {
     Column(
-        modifier = Modifier
-            .fillMaxSize()
-            .verticalScroll(rememberScrollState()),
-        horizontalAlignment = Alignment.CenterHorizontally
+        Modifier.padding(
+            start = 16.dp,
+            end = 16.dp,
+            top = 16.dp
+        )
     ) {
-        SearchBar(
-            modifier = Modifier.fillMaxWidth(),
-            inputField = {
-                SearchBarDefaults.InputField(
-                    query = "",
-                    onQueryChange = { },
-                    onSearch = { expanded = false },
-                    expanded = expanded,
-                    onExpandedChange = { expanded = it },
-                    placeholder = { Text("Buscar película...") }
-                )
+        TextField(
+            value = query,
+            onValueChange = onQueryChange,
+            placeholder = { Text("Search movies...") },
+            leadingIcon = {
+                Icon(Icons.Default.Search, contentDescription = null)
             },
-            expanded = expanded,
-            onExpandedChange = { expanded = it },
-        ) {
-            Column(
-                modifier = Modifier.fillMaxWidth()
-            ) {
-                ListItem(
-                    headlineContent = { Text("Matrix") },
-                    modifier = Modifier.fillMaxWidth()
-                )
-                ListItem(
-                    headlineContent = { Text("Inception") },
-                    modifier = Modifier.fillMaxWidth()
-                )
-                ListItem(
-                    headlineContent = { Text("Interstellar") },
-                    modifier = Modifier.fillMaxWidth()
-                )
-            }
-        }
+            shape = RoundedCornerShape(25),
+            modifier = Modifier
+                .fillMaxWidth()
+                .height(56.dp),
+            colors = TextFieldDefaults.colors(
+                focusedContainerColor = Color(0xFF292938),
+                unfocusedContainerColor = Color(0xFF292938),
+                focusedIndicatorColor = Color.Transparent,
+                unfocusedIndicatorColor = Color.Transparent
+            )
+        )
     }
 }
